ef049850bf270dc1ff8430e327b5f19e9c885a31
Skullyfox <Skullyfox.yt@gmail.com>
2019-06-16 19:37:22 +0200

    fix error

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -2,6 +2,6 @@ import io from 'socket.io-client';
 
 let socket;
 
-return (process.env.NODE_ENV === 'dev') ? socket = io.connect(`http://localhost:3001/`) : socket = io.connect(`https://chat-react-socketio.herokuapp.com`);
+(process.env.NODE_ENV === 'dev') ? socket = io.connect(`http://localhost:3001/`) : socket = io.connect(`https://chat-react-socketio.herokuapp.com`);
 
 export default socket
